1f97537063b5972f070418bf0fca916068061420
Rogelio <rogeliogalicia@Rogelios-MacBook-Air.local>
2021-08-17 00:23:34 -0500

    first commit with boilerplate

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2020 by Stefan Hall, Adam Kennedy, and Rogelio Galicia</div>
+    </footer>
+  );
+};
+
+export default Footer;
