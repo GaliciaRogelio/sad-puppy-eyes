3428cea0e7f1504b43b4253243f68624ba2d1c84
stefan hall <stefhall@comcast.net>
2021-08-28 16:43:53 -0700

    Adding ability to call stripe API to create product in resolvers.js. Made sure the login/ other functionality still works before committing change

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -45,51 +45,36 @@ const resolvers = {
         .populate("payments")
         .populate("thoughts");
     },
+    checkout: async (parent, args, context) => {
+      const order = new Order({ products: args.products });
+      const { products } = await order.populate('products').execPopulate();
+
+      const line_items = [];
+
+      for (let i = 0; i < products.length; i++) {
+      // generate product id
+      const product = await stripe.products.create({
+        name: products[i].name,
+        description: products[i].description
+     });
+
+  // generate price id using the product id
+  const price = await stripe.prices.create({
+    product: product.id,
+    unit_amount: products[i].price * 100,
+    currency: 'usd',
+  });
+
+  // add price id to the line items array
+  line_items.push({
+    price: price.id,
+    quantity: 1
+  });
+}
+    },
     
  
-    // addCard: async (parent, { cardNumber }) => {
-    //   return addCard.findOne({ cardNumber });
-      
-    // },
 
-
-    // checkout: async (parent, args, context) => {
-    //   const url = new URL(context.headers.referer).origin;
-    //   const order = new Order({ products: args.products });
-    //   const { products } = await order.populate('products').execPopulate();
-    //   const line_items = [];
-
-    //   for (let i = 0; i < products.length; i++) {
-    //     // generate product id
-    //     const product = await stripe.products.create({
-    //       name: products[i].name,
-    //       description: products[i].description,
-    //       images: [`${url}/images/${products[i].image}`]
-    //     });
-
-    //     // generate price id using the product id
-    //     const price = await stripe.prices.create({
-    //      product: product.id,
-    //      unit_amount: products[i].price * 100,
-    //      currency: 'usd',
-    //     });
-
-    //     // add price id to the line items array
-    //    line_items.push({
-    //       price: price.id,
-    //       quantity: 1
-    //     });
-    //   }
-    //   const session = await stripe.checkout.sessions.create({
-    //     payment_method_types: ['card'],
-    //     line_items,
-    //     mode: 'payment',
-    //     success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
-    //     cancel_url: `${url}/`
-    //   });
-      
-    //   return { session: session.id };
-    // },
   },
   Mutation: {
     addUser: async (parent, args) => {
@@ -98,12 +83,7 @@ const resolvers = {
 
       return { token, user };
     },
-    // addCard: async (parent, args) => {
-    //   const addCard = await addCard.create(args);
-
-    //   return { addCard };
-    // },
-
+  
    
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
